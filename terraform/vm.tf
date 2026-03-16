3c7a9de36388d78cee3a12ac7f5e5b1326aa403d
asabate <asabate@bsc.es>
2026-03-16 19:34:48 +0100

    finished vm role

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -1,6 +1,6 @@
 resource "azurerm_network_security_group" "security_group" {
   name                = "example-security-group"
-  location            = azurerm_resource_group.rg.location
+  location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   
   security_rule {
@@ -30,7 +30,7 @@ resource "azurerm_network_security_group" "security_group" {
 
 resource "azurerm_virtual_network" "network" {
   name                = "example-network"
-  location            = azurerm_resource_group.rg.location
+  location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
 #  dns_servers         = ["10.0.0.4", "10.0.0.5"]
@@ -61,14 +61,14 @@ resource "azurerm_subnet_network_security_group_association" "assoc" {
 resource "azurerm_public_ip" "public_ip" {
 	name                = "public_ip_vm"
 	resource_group_name = azurerm_resource_group.rg.name
-	location            = azurerm_resource_group.rg.location
+	location            = var.location
 	allocation_method   = "Static"
 	sku		    = "Standard"
 }
 
 resource "azurerm_network_interface" "interface" {
   name                = "interface"
-  location            = azurerm_resource_group.rg.location
+  location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
@@ -84,9 +84,9 @@ resource "azurerm_network_interface" "interface" {
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "vm-CP2"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  location            = var.location
   size                = "Standard_B2ats_v2"
-  admin_username      = "adminuser"
+  admin_username      = var.ssh_user
   network_interface_ids = [
     azurerm_network_interface.interface.id,
   ]
